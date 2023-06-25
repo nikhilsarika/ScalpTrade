bbc592e95ad9a977d11bf321c42fb2dedd6215c3
nikhilsarika <nsarika@hawk.iit.edu>
2023-06-25 10:34:23 -0500

    Modified Process Message Function

--- ScalpTrade/ScalpTrade.cpp
+++ ScalpTrade/ScalpTrade.cpp
@@ -11,6 +11,7 @@
 #include <thread>
 #include <atomic>
 
+#include <typeinfo>
 
 #include <array>
 #include <memory>
@@ -28,13 +29,16 @@ std::atomic<int> vwap_numerator(0);
 std::atomic<int> vwap_denominator(0);
 std::atomic<int> vwap(0);
 std::atomic<bool> triggerVwapUpdate(true);
-std::atomic<bool> vwapFlag(true);
+std::atomic<bool> vwapFlag(false);
 Quote* newQuote = nullptr;
 Trade* newTrade = nullptr;
 ConcurrentQueue<Trade> *tradeQueue = new ConcurrentQueue<Trade>();
-void processMessage(byte* input, string const& orderSide, int const& vwapWindow, int const& maxQuantity);
+void processQuote(Quote* newQuote, string const& orderSide, int const& maxQuantity);
+void processTrade(Trade* newTrade, int const& vwapWindow);
+//void processMessage1(byte* input, string const& orderSide, int const& vwapWindow, int const& maxQuantity);
 void placeNewOrder(string symbol, char side, uint32_t quantity, int32_t price);
 void updateVwap(int const& vwapWindow);
+template< typename T > std::array< byte, sizeof(T) >  to_bytes(const T& object);
 int main(int argc, char* argv[]) {
 	
 
@@ -60,37 +64,53 @@ int main(int argc, char* argv[]) {
 	//Test Data Generation to Stream to the 
 	MessageHeader* header = new MessageHeader(9, 1);
 	Quote* quote = new Quote(header, "IBM", 1580152659000000000ULL, 5, 13897, 9, 13898);
-	Trade* trade = new Trade(header, "IBM", 1580152659000000000ULL, 100, 13900);
+
+	MessageHeader* header1 = new MessageHeader(9, 2);
+	Trade* trade = new Trade(header1, "IBM", 1580152659000000000ULL, 100, 13900);
 
 
 	//auto quoteBytes = to_bytes(*quote);
 	//auto tradeBytes = to_bytes(*trade);
 
-	//Quote* newQuote = reinterpret_cast<Quote*>(&quoteBytes);
-	//unsigned int length = newQuote->header->length;
-	//unsigned int type = newQuote->header->type;
-	//string quote_symbol = newQuote->symbol;
-	//unsigned long long int timestamp = newQuote->timestamp;
-	//unsigned int  bidQuantity = newQuote->bidQuantity;
-	//unsigned int  bidPrice = newQuote->bidPrice;
-	//unsigned int  askQuantity = newQuote->askQuantity;
-	//int askPrice = newQuote->askPrice;
+	auto bytes = to_bytes(*quote);
 
-	//cout << length << "\n";
-	//cout << type << "\n";
-	//cout << quote_symbol << "\n";
-	//cout << std::dec << timestamp << "\n";
-	//cout << bidQuantity << "\n";
-	//cout << std::dec << bidPrice << "\n";
-	//cout << askQuantity << "\n";
-	//cout << std::dec << askPrice << "\n";
+	cout << "Type of bytes : " << typeid(bytes).name() << '\n';
+
+	Quote* newQuote = reinterpret_cast<Quote*>(&bytes);
+	unsigned int length = newQuote->header->length;
+	unsigned int type = newQuote->header->type;
+	string quote_symbol = newQuote->symbol;
+	unsigned long long int timestamp = newQuote->timestamp;
+	unsigned int  bidQuantity = newQuote->bidQuantity;
+	unsigned int  bidPrice = newQuote->bidPrice;
+	unsigned int  askQuantity = newQuote->askQuantity;
+	int askPrice = newQuote->askPrice;
+
+	cout << length << "\n";
+	cout << type << "\n";
+	cout << quote_symbol << "\n";
+	cout << std::dec << timestamp << "\n";
+	cout << bidQuantity << "\n";
+	cout << std::dec << bidPrice << "\n";
+	cout << askQuantity << "\n";
+	cout << std::dec << askPrice << "\n";
 
 	//Implement Socket to receive Byte data;
 	//processMessage(input, orderSide, vwapWindow, maxQuantity);
 	//Below quoteBytes and Tradebytes are sent by the socket
-	processMessage(reinterpret_cast<byte*>(&quote), orderSide, vwapWindow, maxOrderSize);
-	processMessage(reinterpret_cast<byte*>(&trade), orderSide, vwapWindow, maxOrderSize);
-
+	
+	while (true) {
+		cout << "Inside while" << vwap << '\n';
+		//to_bytes(quote);
+		//processMessage(reinterpret_cast<byte*>(&trade), orderSide, vwapWindow, maxOrderSize);
+		//processMessage(reinterpret_cast<byte*>(&quote), orderSide, vwapWindow, maxOrderSize);
+		if (vwapFlag) {
+			processQuote(quote, orderSide, maxOrderSize);
+		}
+		
+		processTrade(trade, vwapWindow);
+	}
+	   
 	return 0;
 }
 
@@ -101,28 +121,60 @@ int main(int argc, char* argv[]) {
 //	return begin;
 //}
 
+template< typename T > std::array< byte, sizeof(T) >  to_bytes(const T& object)
+{
+	std::array< byte, sizeof(T) > bytes;
 
-void processMessage(byte* input, string const& orderSide, int const& vwapWindow, int const& maxQuantity) {
+	const byte* begin = reinterpret_cast<const byte*>(std::addressof(object));
+	const byte* end = begin + sizeof(T);
+	std::copy(begin, end, std::begin(bytes));
 
-	MessageHeader* messageHeader = reinterpret_cast<MessageHeader*>(*input);
+	return bytes;
+}
 
-	if (vwapFlag && messageHeader->type == 1) {
-		newQuote = reinterpret_cast<Quote*>(&input);
-		if (orderSide == "B" && static_cast<int>(newQuote->bidPrice) < vwap) {
-			placeNewOrder(newQuote->symbol, 'B', max(maxQuantity, static_cast<int>(newQuote->bidQuantity)), newQuote->bidPrice);
-		}
-		else if (orderSide == "S" && static_cast<int>(newQuote->askPrice) > vwap) {
-			placeNewOrder(newQuote->symbol, 'S', max(maxQuantity, static_cast<int>(newQuote->askQuantity)), newQuote->askPrice);
-		}
+//template< typename T >
+//void processMessage(std::array< byte, sizeof(T) > input, string const& orderSide, int const& vwapWindow, int const& maxQuantity) {
+//
+//	MessageHeader* messageHeader = reinterpret_cast<MessageHeader*>(&input);
+//	cout << "reached processMessage" << vwap<< '\n';
+//	if (vwapFlag && messageHeader->type == 1) {
+//		newQuote = reinterpret_cast<Quote*>(&input);
+//		if (orderSide == "B" && static_cast<int>(newQuote->bidPrice) < vwap) {
+//			placeNewOrder(newQuote->symbol, 'B', max(maxQuantity, static_cast<int>(newQuote->bidQuantity)), newQuote->bidPrice);
+//		}
+//		else if (orderSide == "S" && static_cast<int>(newQuote->askPrice) > vwap) {
+//			cout << "reached processMessage in sell" << vwap << '\n';
+//			placeNewOrder(newQuote->symbol, 'S', max(maxQuantity, static_cast<int>(newQuote->askQuantity)), newQuote->askPrice);
+//		}
+//	}
+//	else if (messageHeader->type == 2) {
+//		newTrade = reinterpret_cast<Trade*>(&input);
+//		tradeQueue->push(*newTrade);
+//		if (triggerVwapUpdate) {
+//			//vwap is updated in the background as the trade messages are received. 
+//			thread vwapThread(updateVwap, vwapWindow);
+//			triggerVwapUpdate = false;
+//		}
+//	}
+//}
+
+void processQuote(Quote* newQuote, string const& orderSide, int const& maxQuantity) {
+	if (orderSide == "B" && static_cast<int>(newQuote->bidPrice) < vwap) {
+		placeNewOrder(newQuote->symbol, 'B', max(maxQuantity, static_cast<int>(newQuote->bidQuantity)), newQuote->bidPrice);
 	}
-	else if (messageHeader->type == 2) {
-		newTrade = reinterpret_cast<Trade*>(&input);
-		tradeQueue->push(*newTrade);
-		if (triggerVwapUpdate) {
-			//vwap is updated in the background as the trade messages are received. 
-			thread vwapThread(updateVwap, vwapWindow);
-			triggerVwapUpdate = false;
-		}
+	else if (orderSide == "S" && static_cast<int>(newQuote->askPrice) > vwap) {
+		cout << "reached processMessage in sell" << vwap << '\n';
+		placeNewOrder(newQuote->symbol, 'S', max(maxQuantity, static_cast<int>(newQuote->askQuantity)), newQuote->askPrice);
+	}
+}
+
+void processTrade(Trade* newTrade,int const& vwapWindow) {
+
+	tradeQueue->push(*newTrade);
+	if (triggerVwapUpdate) {
+		//vwap is updated in the background as the trade messages are received. 
+		thread vwapThread(updateVwap, vwapWindow);
+		triggerVwapUpdate = false;
 	}
 }
 
@@ -153,4 +205,6 @@ void placeNewOrder(string symbol, char side, uint32_t quantity, int32_t price) {
 	Order* newOrder = new Order(symbol, timestamp, side, quantity, price);
 	auto bytes = reinterpret_cast<byte*>(&newOrder);
 	//add code to send new order. 
+	cout << "reached Place new order" << '\n';
+	cout << bytes << '\n';
 }
